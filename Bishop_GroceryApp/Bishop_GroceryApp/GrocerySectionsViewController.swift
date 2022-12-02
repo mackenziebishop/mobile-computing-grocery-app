7f42f1d9aa6e7958e4d24dee443060d13a4002fb
Mackenzie Bishop <69914681+mackenziebishop@users.noreply.github.com>
2022-12-01 19:33:11 -0600

    Add VC's and controllers

---
+++ Bishop_GroceryApp/Bishop_GroceryApp/GrocerySectionsViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Bishop_GroceryApp
+//
+//  Created by Bishop,Mackenzie N on 12/1/22.
+//
+
+import UIKit
+
+class GrocerySectionsViewController: UIViewController {
+
+    @IBOutlet weak var grocerySectionsTableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Bishop_GroceryApp/Bishop_GroceryApp/ItemsInfoViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ItemsInfoViewController.swift
+//  Bishop_GroceryApp
+//
+//  Created by Bishop,Mackenzie N on 12/1/22.
+//
+
+import UIKit
+
+class ItemsInfoViewController: UIViewController {
+
+    @IBOutlet weak var itemImageViewOutlet: UIImageView!
+    
+    @IBOutlet weak var itemInfoOutlet: UITextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func showItemInfoButton(_ sender: Any) {
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
